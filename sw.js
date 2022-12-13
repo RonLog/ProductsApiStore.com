e9dc557c91151ee2efb775e7858f0004fe74c5c9
RonLog <roshan7715singh@gmail.com>
2022-12-13 20:26:09 +0530

    added pwa files

---
+++ sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'pwa';
+var filesToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './main.js',
+    'https://kit.fontawesome.com/a131e7076c.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
