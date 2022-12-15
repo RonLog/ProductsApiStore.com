fd6fd32d69813e85e4afcfd2f3c30e8748804e27
RonLog <roshan7715singh@gmail.com>
2022-12-15 17:49:08 +0530

    added responsive mobile navbar and submenu

--- script.js
+++ script.js
@@ -1,24 +1,105 @@
+// fetch("https://fakestoreapi.com/products")
+//     .then((data) => {
+//         return data.json();
+//     })
+//     .then((completedata) => {
+//         let data1 = "";
+//         completedata.map((values) => {
+//             data1 += `<div class="card">
+//         <h1 class="title">${values.title}</h1>
+//         <img src="${values.image}" alt="img" class="images" />
+//         <p class="category">${values.category}</p>
+//         <p class="rating">${values.rating.rate}☆☆☆☆☆ <span class="count">${values.rating.count} ratings</span></p>
+//         <p class="price">price: <span class="price_value">$ ${price}</span></p>
+//         <div class="addBuy">
+//           <p class="addtocart">Add to Cart</p>
+//           <p class="buynow">Buy Now</p>
+//         </div>
+//     </div>`;
+//             document.getElementById("cards").innerHTML = data1;
+//         });
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+
+
+let cards = document.querySelector(".cards");
+let filterInput = document.getElementById("filterInput");
+
 fetch("https://fakestoreapi.com/products")
-    .then((data) => {
-        return data.json();
-    })
-    .then((completedata) => {
-        let data1 = "";
-        completedata.map((values) => {
-            data1 += `<div class="card">
-        <h1 class="title">${values.title}</h1>
-        <img src="${values.image}" alt="img" class="images" />
-        <p class="category">${values.category}</p>
-        <p class="rating">${values.rating.rate}☆☆☆☆☆ <span class="count">${values.rating.count} ratings</span></p>
-        <p class="price">price: $ ${values.price}</p>
-        <div class="addBuy">
-          <p class="addtocart">Add to Cart</p>
-          <p class="buynow">Buy Now</p>
-        </div>
-    </div>`;
-            document.getElementById("cards").innerHTML = data1;
-        });
-    })
-    .catch((err) => {
-        console.log(err);
+    .then((res) => res.json())
+    .then((json) => {
+        console.log(json);
+        // iterating products
+        for (let value of json) {
+            addElement(cards, value);
+        }
     });
+
+filterInput.addEventListener("keyup", filterProducts);
+
+// callback function
+function filterProducts() {
+    let filterValue = filterInput.value.toUpperCase();
+    let item = cards.querySelectorAll(".item");
+    // console.log(filterValue);
+
+    for (let i = 0; i < item.length; i++) {
+        let span = item[i].querySelector(".title");
+
+        if (span.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            item[i].style.display = "initial";
+        } else {
+            item[i].style.display = "none";
+        }
+    }
+}
+
+// get value from the api create dynamic element
+function addElement(appendIn, value) {
+    let div = document.createElement("div");
+    div.className = "card";
+
+    let { image, title, category, price } = value;
+
+    div.innerHTML = `
+                <h1 class="title">${title}</h1>
+                <img src="${image}" alt="img" class="images" />
+                <p class="category">${category}</p>
+                <p class="price">price: <span class="price_value">$ ${price}</span></p>
+                <div class="addBuy">
+                  <p class="addtocart">Add to Cart</p>
+                  <p class="buynow">Buy Now</p>
+                </div>
+            `;
+    appendIn.appendChild(div);
+}
+
+function filterProducts() {
+    // remove all the childs from the current element
+    while (cards.childNodes.length > 1) {
+        cards.removeChild(cards.lastChild);
+    }
+
+    fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((json) => {
+            let filterValue = filterInput.value.toUpperCase();
+            let filterData = match(json, "title", filterValue);
+
+            for (const value of filterData) {
+                addElement(cards, value);
+            }
+        });
+}
+
+// match
+const match = (values, filterby, input) => {
+    const p = Array.from(input).reduce((a, v, i) => `${a}[^${input.substr(i)}]*?${v}`, '');
+    const re = RegExp(p);
+
+    return values.filter((v) => v[filterby].toUpperCase().match(re));
+    console.log(p);
+};
